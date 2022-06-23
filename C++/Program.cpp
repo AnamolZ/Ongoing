bfcf7e62cbf7021330d55d37b8a8ffdbc6c9e3bd
Anamol <anmoldkl971@gmail.com>
2022-06-23 22:50:47 +0545

    Updating files

--- C++/Program.cpp
+++ C++/Program.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World"<<endl;
-    return 0;
+    char a[10];
+    int i;
+    for(i=0;i<3;i++){
+        cin>>a[i];
+    }
+    cout<<"Reverse"<<endl;
+    for(i=0;i<3;i++){
+        cout<<a[2-i]<<endl;
+    }
+    for(i=0;i<3;i++){
+        cout<<addressof(a[i])<<endl;
+    }
 }
